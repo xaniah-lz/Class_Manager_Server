257091000fc67c95c1e6b0f9e7c48b74c07f4612
xaniah-lz <163997528+xaniah-lz@users.noreply.github.com>
2025-09-16 11:21:27 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-const SERVER_URL = "https://bsit-4b-classmanagement-server.vercel.app/users"
+const SERVER_URL = "https://zorrilla-classmanagement-server.vercel.app/users"
 
 
 
@@ -177,4 +177,5 @@ const styles = StyleSheet.create({
     gap: 150,
   },
 });
+
 
